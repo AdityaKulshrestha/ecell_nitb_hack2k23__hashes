b5513f007aef0c88e044d0f39c78b733536f15d4
Prafull26 <2100520310044@ietlucknow.ac.in>
2023-02-12 09:40:28 +0530

    fifth commit

--- lib/reward_page.dart
+++ lib/reward_page.dart
@@ -1,8 +1,7 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 
 class Reward_page extends StatefulWidget {
   const Reward_page({super.key});
@@ -14,58 +13,156 @@ class Reward_page extends StatefulWidget {
 class _Reward_pageState extends State<Reward_page> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: const Center(
-          child: Text(
-            'Profile',
-            style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 148, 133)),
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.white,
+                Color.fromARGB(255, 0, 148, 133),
+              ],
+              begin: FractionalOffset.bottomCenter,
+              end: FractionalOffset.topCenter,
+            ),
           ),
         ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 100,
-                height: 100,
-                margin: const EdgeInsets.only(left: 20, top: 20),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: AssetImage('img/images/tushar.jpg'),
-                      fit: BoxFit.fill),
-                ),
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Column(
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 73),
+              child: Column(
                 children: [
+                  const Text(
+                    'My Profile',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 6, 49, 52),
+                      fontSize: 40,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   Container(
-                    width: 150,
-                    height: 60,
-                    margin: const EdgeInsets.only(left: 20, top: 20),
-                    child: const Text(
-                      "Tushar Sharma",
-                      style: TextStyle(
-                          fontSize: 50,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                    height: height * 0.43,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        double innerHeight = constraints.maxHeight;
+                        double innerWidth = constraints.maxWidth;
+                        return Stack(
+                          fit: StackFit.expand,
+                          children: [
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Container(
+                                height: innerHeight * 0.75,
+                                width: innerWidth,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(35),
+                                  color: Colors.white,
+                                ),
+                                child: Column(
+                                  children: [
+                                    const SizedBox(
+                                      height: 90,
+                                    ),
+                                    const Text(
+                                      'Tushar Sharma',
+                                      style: TextStyle(
+                                        color: Color.fromRGBO(27, 181, 216, 1),
+                                        fontFamily: 'Nunito',
+                                        fontSize: 37,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Column(
+                                          children: const [
+                                            Center(
+                                              child: Text(
+                                                'Credits',
+                                                style: TextStyle(
+                                                  color: Color.fromARGB(
+                                                      255, 0, 0, 0),
+                                                  fontFamily: 'Nunito',
+                                                  fontSize: 25,
+                                                ),
+                                              ),
+                                            ),
+                                            SizedBox(
+                                              height: 5,
+                                            ),
+                                            Center(
+                                              child: Text(
+                                                '15',
+                                                style: TextStyle(
+                                                  color: Color.fromRGBO(
+                                                      27, 181, 216, 1),
+                                                  fontFamily: 'Nunito',
+                                                  fontSize: 28,
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              top: 0,
+                              left: 0,
+                              right: 0,
+                              child: Center(
+                                child: Container(
+                                  
+                                  // decoration: BoxDecoration(
+                                  //     image: DecorationImage(
+                                  //         image: AssetImage(
+                                  //             'img/images/tushar.jpg')),
+                                  //     shape: BoxShape.circle,
+                                  //     boxShadow: [
+                                  //       BoxShadow(
+                                  //           color: Color.fromARGB(
+                                  //               255, 0, 148, 133))
+                                  //     ]),
+                                  child: Image.asset(
+                                    'img/images/tushar.jpg',
+                                    width: innerWidth * 0.40,
+                                    fit: BoxFit.fill,
+                                    
+                                    
+                                  ),
+                                  
+                                ),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
                     ),
                   ),
                 ],
-              )
-            ],
-          )
-        ],
-      ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
